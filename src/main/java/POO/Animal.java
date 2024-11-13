bf930f64a4844e82f55e141e5c2cda1a2cfb666c
Dell 5421 <Dell 5421@Funes>
2024-11-13 15:26:57 -0600

    herencias

---
+++ src/main/java/POO/Animal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POO;
+
+/**
+ *
+ * @author Dell 5421
+ */
+public class Animal {
+    
+}
---
+++ src/main/java/POO/JavaClase.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package POO;
+
+/**
+ *
+ * @author Dell 5421
+ */
+public class JavaClase {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
