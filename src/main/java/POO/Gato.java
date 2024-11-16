8a1a699f0267117fad7d97d7b0f37db63f4003b3
Dell 5421 <Dell 5421@Funes>
2024-11-16 17:05:59 -0600

    herencias

---
+++ src/main/java/POO/Gato.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POO;
+
+/**
+ *
+ * @author Dell 5421
+ */
+public class Gato extends Animal {
+    
+    public Gato(String pelo, String boca, String cola, String ojo, Integer patas) {
+        super(pelo, boca, cola, ojo, patas);
+    }
+    public void sonido(){
+        System.out.println("El GAto maulla");
+                
+    }
+    
+    
+}
---
+++ src/main/java/POO/Perro.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POO;
+
+/**
+ *
+ * @author Dell 5421
+ */
+public class Perro extends Animal {
+    
+    public Perro(String pelo, String boca, String cola, String ojo, Integer patas) {
+        super(pelo, boca, cola, ojo, patas);
+    }
+  
+    
+    public void sonido(){
+        System.out.println("El perro Ladra"); 
+    }
+    
+    
+}
