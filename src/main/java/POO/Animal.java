fd36305a56a132d7f5d53e39544ea583d1656100
Dell 5421 <Dell 5421@Funes>
2024-11-16 17:05:10 -0600

    herencias

--- src/main/java/POO/Animal.java
+++ src/main/java/POO/Animal.java
@@ -9,5 +9,62 @@ package POO;
  * @author Dell 5421
  */
 public class Animal {
+    private String pelo; 
+    private String boca;
+    private String cola;
+    private String ojo;
+    private Integer patas; 
+
+    public Animal(String pelo, String boca, String cola, String ojo, Integer patas) {
+        this.pelo = pelo;
+        this.boca = boca;
+        this.cola = cola;
+        this.ojo = ojo;
+        this.patas = patas;
+    }
+
+    public String getPelo() {
+        return pelo;
+    }
+
+    public void setPelo(String pelo) {
+        this.pelo = pelo;
+    }
+
+    public String getBoca() {
+        return boca;
+    }
+
+    public void setBoca(String boca) {
+        this.boca = boca;
+    }
+
+    public String getCola() {
+        return cola;
+    }
+
+    public void setCola(String cola) {
+        this.cola = cola;
+    }
+
+    public String getOjo() {
+        return ojo;
+    }
+
+    public void setOjo(String ojo) {
+        this.ojo = ojo;
+    }
+
+    public Integer getPatas() {
+        return patas;
+    }
+
+    public void setPatas(Integer patas) {
+        this.patas = patas;
+    }
+
+    void sonido() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
